7f2483fedef18666bbf38be28c73e62ee3a4dcb3
Daniil Okhlopkov <5613295+ohld@users.noreply.github.com>
2023-05-17 16:59:46 +0100

    add @flow, @task and logger example

--- flows/github/users.py
+++ flows/github/users.py
@@ -1,11 +1,18 @@
 import httpx
-from prefect import flow
+from prefect import flow, task, get_run_logger
 
-@flow
-async def get_user_info(username):
+@task
+async def get_user_info(username: str) -> dict:
     async with httpx.AsyncClient(timeout=10) as client:
         resp = await client.get(f"https://api.github.com/users/{username}")
         resp.raise_for_status()
 
     return resp.json()
 
+
+@flow
+async def analyse_github_user(username: str):
+    github_user_info = await get_user_info(username)
+
+    logger = get_run_logger()
+    logger.info(f"""@{username}'s id = {github_user_info["id"]}""")
